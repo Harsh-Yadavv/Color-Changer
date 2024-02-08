87fc93d4c8d4124e72413925465bf54a04ff3cb0
Harsh Yadav <145007304+Harsh-Yadavv@users.noreply.github.com>
2024-02-08 16:57:53 +0200

    Add files via upload

---
+++ Projec1.js
@@ -0,0 +1,36 @@
+const buttons=document.querySelectorAll('button')
+const body=document.querySelector("body")
+
+buttons.forEach(function(button){
+    button.addEventListener("click",function(e){
+        console.log(e);
+        console.log(e.target);
+        if (e.target.id==='grey') {
+            body.style.backgroundColor=e.target.id
+        }
+        if (e.target.id==='yellow') {
+            body.style.backgroundColor=e.target.id
+        }
+        if (e.target.id==='blue') {
+            body.style.backgroundColor=e.target.id
+        } 
+        if (e.target.id==='white') {
+            body.style.backgroundColor=e.target.id
+        }
+        if (e.target.id==='purple') {
+            body.style.backgroundColor=e.target.id
+        } 
+        if (e.target.id==='red') {
+            body.style.backgroundColor=e.target.id
+        } 
+        if (e.target.id==='green') {
+            body.style.backgroundColor=e.target.id
+        } 
+        if (e.target.id==='violet') {
+            body.style.backgroundColor=e.target.id
+        } 
+         else {
+            body.backgroundColor='white'
+        }
+    })
+})
